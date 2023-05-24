cd211a8fdb98666e73ff6c3fc8e3bf1a2f99a801
dominic lizarraga <dominiclizarraga@hotmail.com>
2023-05-23 19:36:48 -0700

    add nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :elevators
+  root 'contracts#index'
+
   resources :task_types
-  resources :contracts
+  
+  resources :contracts do
+    resources :elevators
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
