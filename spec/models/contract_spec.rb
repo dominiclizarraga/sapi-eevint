485e8da43a170bfdad53ae12fd2e3d61497232d8
dominic lizarraga <dominiclizarraga@hotmail.com>
2023-07-05 12:49:36 -0400

    add contract model test

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -24,5 +24,37 @@
 require 'rails_helper'
 
 RSpec.describe Contract, type: :model do
-  # it { should validate_presence_of(:job_name) }
+
+  def build_contract(attributes = {})
+    Contract.new(
+      job_name: 'Sample Job',
+      job_number: '12345',
+      created_at: Time.now,
+      updated_at: Time.now,
+      actual_end_at: Time.now,
+      actual_start_at: Time.now,
+      customer_name: 'John Doe',
+      eng_required_date_at: Time.now,
+      selling_price: 1000,
+      work_status: 1,
+      entry_date: Date.today,
+      weeks_estimate: 4,
+      weeks_engineering: 2,
+      **attributes
+    )
+  end
+
+  describe 'validations' do
+    it 'with all valid attributes' do
+      contract = build_contract
+      expect(contract.valid?).to be_truthy
+    end
+
+    it 'contract with invalid job_number' do
+      contract = Contract.new(job_number: nil)
+      expect(contract.valid?).to be_falsey
+      expect(contract.errors[:job_number]).to include("can't be blank")
+    end
+  end
+
 end
---
+++ spec/requests/contracts_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Contracts", type: :request do
+  # it { should validate_presence_of(:job_name) }
+  pending "11 add some examples to (or delete) #{__FILE__}"
+
+end
