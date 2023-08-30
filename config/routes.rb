edf8710c704b77613b035728ed09001a3ea40274
dominic lizarraga <dominiclizarraga@hotmail.com>
2023-08-29 17:15:01 -0700

    add home route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'contracts#index'
+  root to: 'contracts#home'
   get "/work_statuses", to: 'contracts#work_statuses'
   get "/planning", to: 'contracts#planning'
   resources :contracts do
